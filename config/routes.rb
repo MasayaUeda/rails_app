e443e29b51e8a490df8d6c825baf4a8963e0f218
github masayaueda <ma.ma.masaya16@gmail.com>
2020-01-22 14:11:30 +0900

    revise home controller to homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-	root 'home#home'
-	get 'home/home'
-	get '/about' => 'home#about'
+	get 'users/new'
+
+	root 'homes#home'
+	get '/' => 'homes#home'
+	get '/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
